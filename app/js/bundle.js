5851305927e7b98a05f1944eae1c3a62cd779e58
CRARGER-ПК\CRARGER <CRARGER@yandex.ru>
2018-07-02 12:33:59 +0300

    init

---
+++ app/js/bundle.js
@@ -0,0 +1,13 @@
+//= ../libs/jquery/dist/jquery.min.js
+
+//= ../libs/bootstrap/dist/js/bootstrap.min.js
+
+//= ../libs/owl.carousel/dist/owl.carousel.min.js
+
+//= ../libs/input-mask/jquery.inputmask.bundle.min.js
+
+//= ../libs/jquery-countTo/jquery.countTo.js
+
+//= ../js/script.js
+
+//= ../js/map.js
